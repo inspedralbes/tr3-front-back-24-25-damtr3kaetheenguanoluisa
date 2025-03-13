d9dff61d7772518f03d4670e01c9aa2e8a94e7c3
a23kaeguapio <a23kaeguapio@inspedralbes.cat>
2025-03-13 23:47:59 +0100

    connected to mongoDB

--- back/index.js
+++ back/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 const cors = require('cors');
-const PORT = require('PORT');
-
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 app.use(cors());
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connectat a MongoDB'))
+.catch((err) => console.error('Error al connectar a MongoDB', err));
 
 app.get('/', (req, res) => {
     res.json('Benvingut al back');
