efac34af55584b9a4cbd2326d881332fe02e7d26
a23kaeguapio <a23kaeguapio@inspedralbes.cat>
2025-04-03 02:19:58 +0200

    prod

--- front-end/tr3-UnityProject/ecosystem.config.cjs
+++ front-end/tr3-UnityProject/ecosystem.config.cjs
@@ -1,13 +1,13 @@
-// module.exports = {
-//     apps: [
-//       {
-//         name: 'NuxtAppName',
-//         port: 22212,
-//         NITRO_HOST: '0.0.0.0',
-//         exec_mode: 'cluster',
-//         instances: 'max',
-//         script: './.output/server/index.mjs'
-//       }
-//     ]
-//   }
+module.exports = {
+    apps: [
+      {
+        name: 'NuxtAppName',
+        port: 22212,
+        NITRO_HOST: '0.0.0.0',
+        exec_mode: 'cluster',
+        instances: 'max',
+        script: './.output/server/index.mjs'
+      }
+    ]
+  }
   
